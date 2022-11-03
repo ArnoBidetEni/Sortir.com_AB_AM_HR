9ee816616ec760f141e29199930c3faf94c0db2f
Arno BIDET <arno.bidet2021@campus-eni.fr>
2022-11-03 11:20:07 +0100

    dev[ADD] : Ajout visuel pour modification photo de profil

--- src/app/core/participant/features/participant-display/participant-display.component.ts
+++ src/app/core/participant/features/participant-display/participant-display.component.ts
@@ -13,7 +13,7 @@ import { ParticipantAddComponent } from '../../ui/participant-add/participant-ad
 })
 export class ParticipantDisplayComponent implements OnInit {
   @ViewChild(ParticipantAddComponent, { static: true }) participantAddComponent!: ParticipantAddComponent;
-
+  file: File | null = null;
   participantId$: BehaviorSubject<number> = new BehaviorSubject<number>(-1);
   participant$: Observable<Participant> = this.participantId$.pipe(
     tap(() => console.log("isTapping")),
@@ -26,6 +26,16 @@ export class ParticipantDisplayComponent implements OnInit {
   ngOnInit(): void {
     this.participantId$.next(parseInt(this.route.snapshot.paramMap.get('participantId') ?? "-1"));
     this.setFormEnabled(false);
+  }
+  onChange({ target }: Event) {
+    let files = (target as HTMLInputElement).files
+    if (!files) return;
+
+    if(files[0].size > 2097152){
+      alert("File is too big!");
+      return;
+   };
+    this.file = files[0];
   }
 
   cancel() {
